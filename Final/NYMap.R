7061d97c8e4e3c0d956d037a34b1369735edfa7a
Adam Douglas <lysanthus@gmail.com>
2018-12-04 22:36:43 -0500

    Incremental update

--- Final/NYMap.R
+++ Final/NYMap.R
@@ -1,6 +1,7 @@
-NYMap <- function(df, x, title = NULL, sub = NULL, leg = NULL){
+NYMap <- function(df, x, title = NULL, sub = NULL, leg = NULL, br = NULL){
   suppressMessages(library(maps))
   suppressMessages(library(RColorBrewer))
+  suppressMessages(library(scales))
   mapData <- map_data("county","new york")
   
   mapped <- inner_join(mapData, df, by = c("subregion" = "county"))
@@ -10,10 +11,10 @@ NYMap <- function(df, x, title = NULL, sub = NULL, leg = NULL){
                  aes(x=long, y = lat, 
                      group = group,
                      fill = cut(as.numeric(mapped[[x]]),
-                                breaks=c(0, seq(25,250,by=25)))), 
+                                breaks=br)), 
                  col = "black") + 
-    scale_fill_brewer(leg, palette = "RdYlGn"
-                      ,direction = -1) +
+    scale_fill_brewer(leg, type = "seq", palette = "RdYlGn"
+                      ,direction = -1, drop = FALSE) +
     coord_fixed(1.3) +
     theme(panel.grid.major = element_blank(), 
           panel.grid.minor = element_blank(),
